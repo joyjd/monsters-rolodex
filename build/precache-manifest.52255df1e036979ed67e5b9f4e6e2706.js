68163c922142c1776b531703bfdd0512b653430f
joyjd <joydas1611@gmail.com>
2020-07-29 01:34:22 +0530

    commit

---
+++ build/precache-manifest.52255df1e036979ed67e5b9f4e6e2706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386ac316b29da3f27f262b1d8e521fba",
+    "url": "/testDeploy/index.html"
+  },
+  {
+    "revision": "9b03937860d57d10c5c8",
+    "url": "/testDeploy/static/css/main.a0c503d1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/testDeploy/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testDeploy/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b03937860d57d10c5c8",
+    "url": "/testDeploy/static/js/main.40a77ea6.chunk.js"
+  },
+  {
+    "revision": "6435101a83edda9a91dc",
+    "url": "/testDeploy/static/js/runtime-main.60c9dab4.js"
+  }
+]);
